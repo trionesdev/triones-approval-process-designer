79d68ebb7ef48f809034685cf8a1f583db9d5ee3
Bane.Shi <fengxiaotx@163.com>
2023-12-18 11:04:43 +0800

    feat:

--- examples/approval-process-designer-react/src/activities/ApprovalActivity.tsx
+++ examples/approval-process-designer-react/src/activities/ApprovalActivity.tsx
@@ -4,19 +4,40 @@ import {
     DesignerCore
 } from "@trionesdev/approval-process-designer-react";
 import createResource = DesignerCore.createResource;
-import {Drawer} from "antd";
+import {Button, Drawer, Form, Input} from "antd";
 import {useState} from "react";
 
 export const ApprovalActivity: ActivityFC<any> = ({...props}) => {
+    const [form] = Form.useForm()
+
     const [open, setOpen] = useState(false)
+
 
     const handleClick = () => {
         setOpen(true)
     }
 
+    const handleSave = () => {
+        form.validateFields().then((values: any) => {
+
+        })
+    }
+
     return <>
         <TdApprovalActivity {...props} onClick={handleClick}/>
-        <Drawer open={open}></Drawer>
+        <Drawer open={open} onClose={() => {
+            setOpen(false)
+        }}
+                footer={<div>
+                    <Button onClick={handleSave}>确定</Button>
+                </div>}
+        >
+            <Form form={form}>
+                <Form.Item label={`描述`} name={`description`}>
+                    <Input/>
+                </Form.Item>
+            </Form>
+        </Drawer>
     </>
 }
 
